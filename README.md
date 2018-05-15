c03f925e63fbd8dd2b284375b7f98eef6c976bb0
Ritika Mehta <rimehta@iu.edu>
2018-05-15 10:42:01 -0400

    Add multiple names, clearing the input each time

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ The first project in Xtern Bootcamp 2018.
 This project demonstrates the following:
 
 * Handling events in JavaScript
+ 
--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const form = document.querySelector('form#userForm')
      const handleSubmit = function(ev) {
         ev.preventDefault()
         const form = ev.target
-        const favoriteanimal = form.favoriteanimal.value
-        const heading = document.querySelector('h1')
-        heading.textContent = favoriteanimal
+        const userName = form.userName.value
+        const users = document.querySelector('#users')
+        users.textContent +=" " + userName
+        form.userName.value = " "
     }
 form.addEventListener('submit', handleSubmit)
