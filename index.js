03eef3fa2293f146a87442bea9c71fcb5b2070c5
Ritika Mehta <rimehta@iu.edu>
2018-05-15 14:59:12 -0400

    create a new function renderColor hw 2

--- index.js
+++ index.js
@@ -8,11 +8,45 @@ const form = document.querySelector('form#userForm')
         ev.preventDefault()
         const form = ev.target
         const userName = form.userName.value
+        const age = form.age.value
+        const favoriteColor = form.favoriteColor.value
+
         const users = document.querySelector('#users')
 
-        const p = document.createElement('p')
-        p.textContent = `${userName},${age}`
-        users.appendChild()
+        const list = document.createElement('ul')
+
+        const nameItem = document.createElement('li')
+        nameItem.textContent = `Name: ${userName}`
+
+        const ageItem = document.createElement('li')
+        ageItem.textContent = `Age: ${age}`
+
+        const colorItem = document.createElement('li')
+        colorItem.textContent = 'Favorite Color: '
+
+        const colorDiv = document.createElement('div')
+        // colorDiv.style.backgroundColor= favoriteColor
+        // colorDiv.style.width = '6rem'
+        // colorDiv.style.height = '3rem'
+        // colorItem.appendChild(colorDiv)
+
+        const renderColor = function() {
+        colorDiv.style.backgroundColor = favoriteColor
+        colorDiv.style.width = '6rem'
+        colorDiv.style.height = '3rem'
+        }
+        renderColor()
+        //return colorDiv    
+        colorItem.appendChild(colorDiv)
+           
+        //colorItem.appendChild(renderColor)
+        list.appendChild(nameItem)
+        list.appendChild(ageItem)
+        list.appendChild(colorItem)
+
+
+        users.appendChild(list)
+
         form.reset() 
         form.userName.focus() 
     }
