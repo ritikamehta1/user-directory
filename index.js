63ebf80ca543450d38f845bd926c0c7656363045
Ritika Mehta <rimehta@iu.edu>
2018-05-15 10:01:51 -0400

    updated hw

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const button = document.querySelector('button')
+const form = document.querySelector('form#userForm')
 
-function changeText(){
-    console.log('jjj')
-    let userInput = document.querySelector('.H')
-    userInput.textContent = document.getElementById("FA").value
-    
-}
-button.addEventListener('click', changeText)
+// function changeText(){
+//     console.log('jjj')
+//     let userInput = document.querySelector('.H')
+//     userInput.textContent = document.getElementById("FA").value
+     const handleSubmit = function(ev) {
+        ev.preventDefault()
+        const form = ev.target
+        const favoriteanimal = form.favoriteanimal.value
+        const heading = document.querySelector('h1')
+        heading.textContent = favoriteanimal
+    }
+form.addEventListener('submit', handleSubmit)
