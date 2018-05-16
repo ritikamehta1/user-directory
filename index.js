9c9959cb88b6e48c8e6f720cf9a633c5b08ab2c7
Ritika Mehta <rimehta@iu.edu>
2018-05-15 21:57:42 -0400

    Create a new function called renderListItem

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ const form = document.querySelector('form#userForm')
         const age = form.age.value
         const favoriteColor = form.favoriteColor.value
 
-        const users = document.querySelector('#users')
-
         const list = document.createElement('ul')
-
-        const nameItem = document.createElement('li')
-        nameItem.textContent = `Name: ${userName}`
-
-        const ageItem = document.createElement('li')
-        ageItem.textContent = `Age: ${age}`
-
-        const colorItem = document.createElement('li')
-        colorItem.textContent = 'Favorite Color: '
 
         const colorDiv = document.createElement('div')
         // colorDiv.style.backgroundColor= favoriteColor
@@ -30,17 +19,33 @@ const form = document.querySelector('form#userForm')
         // colorDiv.style.height = '3rem'
         // colorItem.appendChild(colorDiv)
 
-        const renderColor = function() {
+        const renderColor = function(favoriteColor) {
         colorDiv.style.backgroundColor= favoriteColor
         colorDiv.style.width = '6rem'
         colorDiv.style.height = '3rem'
-        colorItem.appendChild(colorDiv)
+        return colorDiv;
         }
-        renderColor()
         
-        list.appendChild(nameItem)
-        list.appendChild(ageItem)
-        list.appendChild(colorItem)
+         function renderListItem () {
+            const nameItem = document.createElement('li')
+            nameItem.textContent = `Name: ${arguments[0]}`
+    
+            const ageItem = document.createElement('li')
+            ageItem.textContent = `Age: ${arguments[1]}`
+    
+            const colorItem = document.createElement('li')
+            colorItem.textContent = 'Favorite Color: '
+            
+            colorItem.appendChild(renderColor(arguments[2]))
+            return [nameItem, ageItem, colorItem]
+        }
+        const users = document.querySelector('#users')
+        
+        const items = renderListItem(userName, age, favoriteColor)
+
+        list.appendChild(items[0])
+        list.appendChild(items[1])
+        list.appendChild(items[2]);
 
 
         users.appendChild(list)
