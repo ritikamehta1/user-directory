9d9cfb6c6c8d3e21484e04d751ead3cfa25c282d
Ritika Mehta <rimehta@iu.edu>
2018-05-16 11:05:41 -0400

    store users in array

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
+
 const form = document.querySelector('form#userForm')
 
-// function changeText(){
-//     console.log('jjj')
-//     let userInput = document.querySelector('.H')
-//     userInput.textContent = document.getElementById("FA").value
      const handleSubmit = function(ev) {
         ev.preventDefault()
         const form = ev.target
-        const userName = form.userName.value
-        const age = form.age.value
-        const favoriteColor = form.favoriteColor.value
-
+        const user = {
+            name: form.userName.value,
+            age: form.age.value,
+            favoriteColor: form.favoriteColor.value,
+        }
         const list = document.createElement('ul')
 
+        Object.keys(user).map(function(label) {
+            const items = renderListItem(label, user[label])        
+            list.appendChild(item)
+        }) 
+
         const colorDiv = document.createElement('div')
-        // colorDiv.style.backgroundColor= favoriteColor
-        // colorDiv.style.width = '6rem'
-        // colorDiv.style.height = '3rem'
-        // colorItem.appendChild(colorDiv)
 
         const renderColor = function(favoriteColor) {
-        colorDiv.style.backgroundColor= favoriteColor
-        colorDiv.style.width = '6rem'
-        colorDiv.style.height = '3rem'
-        return colorDiv;
-        }
+            colorDiv.style.backgroundColor= favoriteColor
+            colorDiv.style.width = '6rem'
+            colorDiv.style.height = '3rem'
+            return colorDiv;
+            }
         
-         function renderListItem () {
+        function renderListItem (label, value) {
             const nameItem = document.createElement('li')
             nameItem.textContent = `Name: ${arguments[0]}`
     
@@ -38,11 +37,10 @@ const form = document.querySelector('form#userForm')
             
             colorItem.appendChild(renderColor(arguments[2]))
             return [nameItem, ageItem, colorItem]
-        }
+            }
+            
         const users = document.querySelector('#users')
         
-        const items = renderListItem(userName, age, favoriteColor)
-
         list.appendChild(items[0])
         list.appendChild(items[1])
         list.appendChild(items[2]);
